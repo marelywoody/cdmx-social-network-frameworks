7ea68829bfc2264dc94ff2df0ec05a0f8420ce37
marelywoody <ros_ely_arc@hotmail.com>
2018-10-04 02:03:26 -0500

    arreglando is login

--- src/app/components/post-wall/post-wall.component.ts
+++ src/app/components/post-wall/post-wall.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -9,9 +10,26 @@ import { AuthenticationService } from '../../services/authentication.service';
 })
 export class PostWallComponent implements OnInit {
 
-  constructor() { }
+  public isLogin: boolean;
+  public nameUser: string;
+  public emailUser: string;
+  public photoUser: string;
+
+  constructor(
+    public authentication: AuthenticationService
+    ) { }
 
   ngOnInit() {
+    this.authentication.stateAuth().subscribe( auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.nameUser = auth.displayName;
+        this.emailUser = auth.email;
+        this.photoUser = auth.photoURL;
+      } else {
+        this.isLogin = false;
+      }
+    });
   }
 
 }
